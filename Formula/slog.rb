c08678fbe938c8869af335ca6bc42aa6b8bc7a75
Patrick Gatewood <patrick@patrickgatewood.com>
2025-10-10 11:44:58 -0400

    Bump slog to 1.1.0 🎉

--- Formula/slog.rb
+++ Formula/slog.rb
@@ -1,8 +1,8 @@
 class Slog < Formula
   desc "Bash-based logging CLI"
   homepage "https://github.com/pg8wood/slog"
-  url "https://github.com/pg8wood/slog/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "ae8b090db3ea66059d5892715d2b706c59b4c6f2df1675e69f7580c477f249a5"
+  url "https://github.com/pg8wood/slog/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "41a76cf1ca6a664371aece3d923b28dc3dbbb1a9c4393aa92d7cdc597bde2262"
 
   head "https://github.com/pg8wood/slog.git", branch: "main"
 
@@ -10,7 +10,7 @@ class Slog < Formula
   depends_on "jq"
 
   def install
-    bin.install "slog"
+    bin.install "slog", "slog-repl.py"
   end
 
   test do
---
+++ Formula/slog@1.0.rb
@@ -0,0 +1,18 @@
+class SlogAT10 < Formula
+  desc "Bash-based logging CLI"
+  homepage "https://github.com/pg8wood/slog"
+  url "https://github.com/pg8wood/slog/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "ae8b090db3ea66059d5892715d2b706c59b4c6f2df1675e69f7580c477f249a5"
+
+  depends_on "cameroncooke/axe/axe"
+  depends_on "jq"
+
+  def install
+    bin.install "slog"
+  end
+
+  test do
+    assert_predicate bin/"slog", :exist?
+    assert_predicate bin/"slog", :executable?
+  end
+end
