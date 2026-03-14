582bd8e3a8e207235c024991a1114923b4111b6b
Patrick Gatewood <patrick@patrickgatewood.com>
2026-03-14 02:25:04 -0400

    Update Mac Lid Lullaby release URL

--- Casks/mac-lid-lullaby.rb
+++ Casks/mac-lid-lullaby.rb
@@ -2,11 +2,11 @@ cask "mac-lid-lullaby" do
   version "1.0"
   sha256 "2c2ed51b2744edfb070f7f74170d763883d2d06a96af3b6657529485e104fa06"
 
-  url "https://github.com/pg8wood/macbook-lid/releases/download/v#{version}/Mac%20Lid%20Lullaby%20#{version}.dmg",
-      verified: "github.com/pg8wood/macbook-lid/"
+  url "https://github.com/pg8wood/mac-lid-lullaby/releases/download/v#{version}/Mac.Lid.Lullaby.#{version}.dmg",
+      verified: "github.com/pg8wood/mac-lid-lullaby/"
   name "Mac Lid Lullaby"
   desc "Menu bar app that plays a custom sound as your MacBook lid closes"
-  homepage "https://github.com/pg8wood/macbook-lid"
+  homepage "https://github.com/pg8wood/mac-lid-lullaby"
 
   depends_on macos: ">= :ventura"
 
